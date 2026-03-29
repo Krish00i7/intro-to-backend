15043590f342de0df0bf17a5b87e84ac0b248c14
Krish00i7 <krishna00i777@gmail.com>
2026-03-29 13:28:31 +0530

    "update"

---
+++ server/src/index.js
@@ -0,0 +1,33 @@
+ import app from "./app.js";
+import connectDB from "./config/database.js";
+import dotenv from "dotenv";
+import dns from "dns";
+
+dotenv.config();
+
+dns.setServers(["1.1.1.1","8.8.8.8"]);
+
+const startServer = async() => {
+    try {
+         await connectDB();
+        console.log("MongoDB URI:", process.env.MONGODB_URI)
+          app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        });
+       
+
+        app.on("error", (error) => {
+            console.log("ERROR", error);
+            throw error;
+        });
+
+      
+
+    } catch(error){
+        console.log("MongoDb connection failed !!! ", error);
+    }
+}
+
+startServer();
+
+
